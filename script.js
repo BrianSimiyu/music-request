21de056fba2ccedf38f514332455b19f606fe1bc
BrianSimiyu <brianmaruti@gmail.com>
2023-05-12 17:19:11 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,63 @@
+// Initialize Firebase
+var firebaseConfig = {
+  apiKey: "AIzaSyA38jw2PMH40_1dhzKBYL-YONidfpVqzlM",
+  authDomain: "music-request-dd100.firebaseapp.com",
+  databaseURL:
+    "https://music-request-dd100-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "music-request-dd100",
+  storageBucket: "music-request-dd100.appspot.com",
+  messagingSenderId: "978464309810",
+  appId: "1:978464309810:web:6a8981c2e9f894c903b22b",
+};
+firebase.initializeApp(firebaseConfig);
+
+// Get a reference to the database service
+var database = firebase.database();
+
+// Get a reference to the request form
+var requestForm = document.getElementById("request-form");
+
+// Get a reference to the request list
+var requestList = document.getElementById("request-list");
+
+// Listen for the form submission
+requestForm.addEventListener("submit", function (event) {
+  // Prevent the default form behavior
+  event.preventDefault();
+
+  // Get the input values
+  var songTitle = requestForm["song-title"].value;
+  var artistName = requestForm["artist-name"].value;
+
+  // Create a new request object with timestamp
+  var timestamp = firebase.database.ServerValue.TIMESTAMP;
+  var newRequestRef = database.ref("requests").push();
+  newRequestRef.set({
+    songTitle: songTitle,
+    artistName: artistName,
+    timestamp: timestamp,
+  });
+
+  // Clear the form inputs
+  requestForm.reset();
+});
+
+// Listen for new requests and display them on the page
+database
+  .ref("requests")
+  .orderByChild("timestamp")
+  .on("child_added", function (snapshot) {
+    var request = snapshot.val();
+    var requestItem = document.createElement("div");
+    requestItem.classList.add("request-item");
+    var timestamp = new Date(request.timestamp).toLocaleTimeString([], {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+    requestItem.innerHTML = `
+    <p class="song-title" >${request.songTitle}</p>
+    <p class="artist-name">${request.artistName}</p>
+    <p class="timestamp">${timestamp}</p>
+  `;
+    requestList.appendChild(requestItem);
+  });
